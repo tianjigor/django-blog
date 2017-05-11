298d6dbc8abace6986cafd222bce78014d3a77f0
Tianjigor <541878782@qq.com>
2017-05-11 23:45:57 +0800

    12. 分类与归档

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from models import Post
+from models import Post, Category
 import markdown
 
 # Create your views here.
@@ -21,3 +21,14 @@ def detail(request, pk):
                                       'markdown.extensions.toc',
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year, created_time__month=month)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
