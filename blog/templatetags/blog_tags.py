d3d8900d8bd16bfd085b71c94394db9341228fca
Tianjigor <541878782@qq.com>
2017-05-15 21:19:00 +0800

    django 博客使用 annotate 统计分类下文章数量

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
-from blog.models import Post, Category
+from blog.models import Post, Category, Tag
 from django import template
+from django.db.models.aggregates import Count
+
 
 register = template.Library()
 
@@ -17,3 +19,13 @@ def archives():
 @register.simple_tag
 def get_categories():
     return Category.objects.all()
+
+
+@register.simple_tag
+def get_categories_count():
+    return Category.objects.annotate(num_posts=Count('post'))
+
+
+@register.simple_tag
+def get_tag_count():
+    return Tag.objects.annotate(num_posts=Count('post'))
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, get_object_or_404
 from models import Post, Category
 from comments.forms import CommentForm
 import markdown
-
 # Create your views here.
 
 def index(request):
